0b89c8d9d60f7b58c1404631f607758f4ac3c136
Mart Lumeste <martlumeste@gmail.com>
2016-12-11 14:17:02 +0200

    Initial Commit

---
+++ wcd2k18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wcd2k18
+//
+//  Created by Mart Lumeste on 11/12/2016.
+//  Copyright © 2016 Mart Lumeste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
